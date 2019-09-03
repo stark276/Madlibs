4d0c842b9ce374c97d1bf0f4a8a36545f85235a1
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2019-09-03 10:39:25 -0700

    updated

---
+++ madlibs.py
@@ -0,0 +1,26 @@
+Things = input("Things: ")
+Food = input("Food: ")
+Thing = input("Thing: ")
+Color = input("Color: ")
+someone_famous = input("Celebrity or someone famous: ")
+Verb = input("Verb(action): ")
+Profession = input("Profession: ")
+Adjective = input("Adjective: ")
+Celebrity = input("A Celebrity: ")
+Place = input("A Place: ")
+Things_plural = input("Things (plural): ")
+Animal = input("Animal: ")
+
+
+
+
+
+
+print("There once was a gingerbread man who had two " + Things + "for eyes and a Food for a nose. He always said, " +
+Verb + "you cannt catch me " + Adjective +
+"Profession, but they couldn't catch him. "
++ Animal + "but they couldnt catch him either. , he came across a river near " +  Place +  "How would he cross? Then he saw a Color " 
++ Thing +
+"floating by. He jumped on it, but it was actually " +
+ Celebrity +  "or " +
+someone_famous + "who just so happened to love cookies ")
