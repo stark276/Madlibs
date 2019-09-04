ad804d4f57e220f9a72018b181f09d413c4c455f
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2019-09-03 23:26:19 -0700

    I have changed my storyh

--- madlibs.py
+++ madlibs.py
@@ -1,26 +1,17 @@
-Things = input("Things: ")
-Food = input("Food: ")
-Thing = input("Thing: ")
-Color = input("Color: ")
-someone_famous = input("Celebrity or someone famous: ")
-Verb = input("Verb(action): ")
-Profession = input("Profession: ")
-Adjective = input("Adjective: ")
-Celebrity = input("A Celebrity: ")
-Place = input("A Place: ")
-Things_plural = input("Things (plural): ")
-Animal = input("Animal: ")
+Things1 = input("Things: ")
+Adjective1 = input("Adjective: ")
+Song = input("Song Title: ")
+Celebrity1 = input("Celebrity: ")
+Feeling1 = input("Feeling: ")
+Verb1 = input("Verb(action): ")
+Place1 = input("Place: ")
+Food1 = input("Food: ")
+Things1 = input("Things: ")
+Person1= input("Person1: ")
 
 
 
-
-
-
-print("There once was a gingerbread man who had two " + Things + "for eyes and a Food for a nose. He always said, " +
-Verb + "you cannt catch me " + Adjective +
-"Profession, but they couldn't catch him. "
-+ Animal + "but they couldnt catch him either. , he came across a river near " +  Place +  "How would he cross? Then he saw a Color " 
-+ Thing +
-"floating by. He jumped on it, but it was actually " +
- Celebrity +  "or " +
-someone_famous + "who just so happened to love cookies ")
+print("I just got back from a pizza party with " + Person1 + "." + "Can you believe we got to eat " + Adjective1 +  "pizza in " + Place1 + "? Everyone got to" + 
+"choose their own toppings. I made ' " + Food1 + "and " + Things1 + "' pizza, which is my favorite! They even stuffed the crust with " + 
+Things1 + "How " + Feeling1 + "! If that wasn't good enough already, A " + Celebrity1 + "was there singing " + Song + ". I was so inspired by " +
+"the music, I had to get up out of my seat and " + Verb1 + ".")
